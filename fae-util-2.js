f2c453762f2f46d8eccae426b83e79b7d0f67c77
Mihir Kumar <mihirshreni@gmail.com>
2018-10-09 14:35:10 -0500

    Depth added

--- fae-util-2.js
+++ fae-util-2.js
@@ -5,6 +5,43 @@ const validUrl = require('valid-url');
 const fs = require('fs');
 const configOptions = require('./config.json');
 
+class Url {
+  constructor(url, depth, parentUrl) {
+    this.url = url;
+    this.depth = depth;
+    this.parentUrl = parentUrl;
+  }
+
+  get getUrl() {
+    return this.url;
+  }
+
+  get getDepth() {
+    return this.depth;
+  }
+
+  get getParentUrl() {
+    return this.parentUrl;
+  }
+}
+
+function directoryIsEmpty(directory) {
+  var flag = false;
+  fs.readdir(directory, function(err, files) {
+    if (err) {
+      console.log(err);
+      flag = false;
+    }
+    else {
+      if (!files.length) {
+        flag = true;
+      }
+    }
+  });
+  console.log('flag', flag);
+  return flag;
+}
+
 function evaluateRules(passedRuleset) {
   console.log('Entered evaluateRules()');
   var doc = window.document;
@@ -21,48 +58,66 @@ function evaluateRules(passedRuleset) {
 
 async function validateLink (url) {
   var flag = false;
-
-  if (validUrl.isUri(url)){
-
+  var URL = url.url;
+  if (validUrl.isUri(URL)){
     flag = true;
-
     for (var x = 0; x < configOptions.excluded_extensions.length && flag; x++){
-      if (url.substr(url.length - 4) == '.' + configOptions.excluded_extensions[x]
-          || url.substr(0, 7) == 'mailto:')
-        flag = false;
+      if (URL.substr(URL.length - 4) == '.' + configOptions.excluded_extensions[x]
+          || URL.substr(0, 7) == 'mailto:'){
+            flag = false;
+            if (URL.substr(URL.length - 4) == '.' + configOptions.excluded_extensions[x])
+            {
+              fs.appendFile(configOptions.outputDirectory + '/excluded_urls.csv', [URL, url.parentUrl, URL.substr(URL.length - 4), '\n'], function (err) {
+                if (err) throw err;
+                console.log('Line 68');
+              });
+
+              fs.appendFile(configOptions.outputDirectory + '/excluded_urls.txt', [URL, '\n'], function (err) {
+                if (err) throw err;
+                console.log('Line 68');
+              });
+            }
+      }
     }
   }
-
   return flag;
 }
 
 async function crawl(urlRoot, numUrlsEvaluated) {
-  console.log('Entered crawl()');
+  console.log('Entered crawl()\n');
   var numPagesEvaluated = 0, currentDepth = 0, index = 0;
-  var currentQueue = [urlRoot];
   var traversed = Array();
   var excluded = Array();
-  var tempUrl;
+  var tempUrl, evaluationStatus;
 
-  while (numPagesEvaluated <= configOptions.maxPages && currentQueue.length) {
+  var urlRootObject = new Url(urlRoot, currentDepth, '');
+  var currentQueue = [urlRootObject];
 
-    console.log('Current Queue: ' + currentQueue);
-
+  while (numPagesEvaluated < configOptions.maxPages && currentQueue.length) {
+    console.log('\nStart of while loop: ');
     tempUrl = currentQueue.shift();
+    console.log('Current Object:',tempUrl.getUrl,tempUrl.depth);
 
-    if (!(tempUrl in traversed)){
-      console.log('Current URL:', tempUrl, 'Current Depth:', currentDepth, 'Index:', index);
+    if (!(tempUrl.url in traversed)){
 
       if (await validateLink(tempUrl)){
-        console.log(tempUrl + ' is a valid URL');
-        traversed.push(tempUrl);
+        console.log(tempUrl.url + ' is a valid URL');
+        // traversed.push(tempUrl);
       } else {
-        console.log(tempUrl + ' is a NOT a valid URL');
-        excluded.push(tempUrl);
+        console.log(tempUrl.url + ' is a NOT a valid URL');
+        excluded.push(tempUrl.url);
         continue;
       }
-  
-      await evaluateSinglePage(tempUrl, currentQueue, currentDepth, index, numUrlsEvaluated);
+      
+      evaluationStatus = await evaluateSinglePage(tempUrl, currentQueue, index, numUrlsEvaluated);
+
+      if (evaluationStatus) {
+        traversed.push(tempUrl.url);
+      }
+      else {
+        excluded.push(tempUrl.url);
+        continue;
+      }
 
       index++;
       numPagesEvaluated++;
@@ -74,24 +129,23 @@ async function crawl(urlRoot, numUrlsEvaluated) {
 
   console.log('Loop end: Traversed:', traversed);
   console.log('Loop end: Excluded:', excluded);
-
-  return;
 }
 
-async function evaluateSinglePage(url, currentQueue, currentDepth, index, numUrlsEvaluated) {
+async function evaluateSinglePage(url, currentQueue, index, numUrlsEvaluated) {
   try {
     console.log('Entered evaluateSinglePage()');
 
     var browser = await puppeteer.launch({
       args: ['--disable-web-security', '--no-sandbox', '--disable-setuid-sandbox']
     });
-
     var page = await browser.newPage();
+    var flag = false;
+    var newUrl;
 
     const millisecondsToSeconds = 1000;
     // load page with/without delay and wait
-    await page.goto(url, { timeout: configOptions.wait * millisecondsToSeconds, waitUntil: 'networkidle0' }); //wait
-    console.log(url, ' successfully navigated to.');
+    await page.goto(url.getUrl, { timeout: configOptions.wait * millisecondsToSeconds, waitUntil: 'networkidle0' }); //wait
+    console.log(url.getUrl, ' successfully navigated to.');
     await page.waitFor(configOptions.delay * millisecondsToSeconds); //delay
 
     // HTTP authentication
@@ -99,7 +153,6 @@ async function evaluateSinglePage(url, currentQueue, currentDepth, index, numUrl
       const credentialsObject = { username: configOptions.username, password: configOptions.password };
       await page.authenticate(credentialsObject);
     }
-    console.log('HTTP auth passed.');
 
     // Import evaluation library into the page
     const evaluationFileOptions = { path: './oaa_a11y_evaluation.js' };
@@ -113,32 +166,36 @@ async function evaluateSinglePage(url, currentQueue, currentDepth, index, numUrl
     await page.addScriptTag(evaluationFileOptionsObject);
     await page.addScriptTag(ruleFileOptionsObject);
     await page.addScriptTag(rulesetsFileOptionsObject);
-    console.log('Evaluation libs injected.');
+    
     //Run evaluation
-
     var results = await page.evaluate(evaluateRules, configOptions.ruleset);
-    console.log('Evaluations completed successfully.');
-    //Export results to file
 
+    if (results) {
+      flag = true;
+      console.log('Evaluation completed');
+    }
+
+    //Export results to file
     fs.writeFile(configOptions.outputDirectory + "/results_" + numUrlsEvaluated.toString() + '_' + index.toString() + ".json", results, function (err) {
       if (err) {
         return console.log(err);
       }
 
-      console.log("results_" + numUrlsEvaluated.toString() + '_' + index.toString() + ".json was saved!");
+      console.log("results_" + url.getDepth.toString() + '_' + index.toString() + ".json was saved!");
     });
 
-    if (currentDepth <= configOptions.depth) {
+    if (url.getDepth < configOptions.depth) {
       // get all links and push at the end of currentQueue
+      console.log('In adding area: ');
       const links = await page.evaluate(() => {
         const anchors = document.querySelectorAll('a');
         return [].map.call(anchors, a => a.href);
       });
-
+      console.log('Passed links');
       for (var i = 0, max = links.length; i < max; i++) {
-        currentQueue.push(links[i]);
+        newUrl = new Url(links[i], url.getDepth+1, url.getUrl);
+        currentQueue.push(newUrl);
       }
-      currentDepth++; 
     }
 
     //Close headless Chrome tab / page
@@ -148,15 +205,31 @@ async function evaluateSinglePage(url, currentQueue, currentDepth, index, numUrl
     console.log(error);
   }
 
-  return true;
+  return flag;
 }
 
 (async () => {
-  //number of URLs from configOptions.urls that have been worked on
-  var numUrlsEvaluated;
+  
+  if (directoryIsEmpty(configOptions.outputDirectory)){
+    console.log('Specified directory not empty. Exiting.');
+    process.exit();
+  }
 
+  else {
+    try {
+      fs.statSync(configOptions.outputDirectory);
+    } catch(e) {
+      fs.mkdirSync(configOptions.outputDirectory);
+    }
+  }
+
+  var numUrlsEvaluated;
+  //number of URLs from configOptions.urls that have been worked on
+    
   for (numUrlsEvaluated = 0; numUrlsEvaluated < configOptions.urls.length; numUrlsEvaluated++) {
     console.log("Run " + numUrlsEvaluated + ":");
     await crawl(configOptions.urls[numUrlsEvaluated], numUrlsEvaluated);
   }
+
+  process.exit(); //Quit entire node script
 })();
